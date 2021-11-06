f3ba036cd06258361c465e0e1a68f4d54dd83682
R3HBeema <r3hbma@gmail.com>
2021-11-06 12:05:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,24 +9,26 @@ function App() {
   const [classes, setClasses] = useState([]);
   const [classesData, setClassesData] = useState([]);
   const [section, setSection] = useState([]);
-  const [sectionData, setSectionData]= useState([]);
+  const [sectionData, setSectionData] = useState([]);
   const [period, setPeriod] = useState([]);
-  const [periodData, setPeriodData]= useState([]);
-  const [area, setArea] = useState([]);
-  const [selarea , setSelArea] = useState([]);
+  const [periodData, setPeriodData] = useState([]);
+  const [areaD, setAreaD] = useState([]);
+  const [selarea, setSelArea] = useState([]);
   const [subject, setSubject] = useState([]);
+  const [typeD, setTypeD] = useState([]);
+  const [studD,setStudD] = useState([]);
 
-  const changeBranchHandler = (e) => {
-    e.preventDefault();
-    setenteredBranch(e.target.value);
-    if (e.target.value === "SLESBP") {
-      setEmail("admin.bp@sas.com");
-    } else if (e.target.value === "SLESKS") {
-      setEmail("admin.Ks@sas.com");
-    } else {
-      setEmail("admin.Mp@sas.com");
-    }
-  };
+  // const changeBranchHandler = (e) => {
+  //   e.preventDefault();
+  //   setenteredBranch(e.target.value);
+  //   if (e.target.value === "SLESBP") {
+  //     setEmail("admin.bp@sas.com");
+  //   } else if (e.target.value === "SLESKS") {
+  //     setEmail("admin.Ks@sas.com");
+  //   } else {
+  //     setEmail("admin.Mp@sas.com");
+  //   }
+  // };
 
   // const getYear = () => {
   //   const baseurl = `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentClass?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}`;
@@ -44,50 +46,65 @@ function App() {
     });
   };
 
-  
-  const getClasses = (enteredBranch,email) => {
-    console.log("yr",enteredBranch,year,email)  
-    
-    const baseurl =
-    `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentClass?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}`;
+  const getClasses = (enteredBranch, email) => {
+    console.log("yr", enteredBranch, year, email);
+
+    const baseurl = `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentClass?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}`;
     axios.get(`${baseurl}`).then((res) => {
       console.log("clD", res);
       setClassesData([...res.data]);
     });
   };
 
-  const getSection = (enteredClass) => {
-    const baseurl = `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentSection?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}&AssessClass=${enteredClass}`;
+  const getSection = (Class) => {
+    const baseurl = `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentSection?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}&AssessClass=${Class}`;
     axios.get(`${baseurl}`).then((res) => {
       console.log("sec ", res);
-      setSectionData([...res.data]);
+      setSectionData([...res.data]);;
     });
   };
 
   const getPeriods = (selectsec) => {
-    const baseurl =
-      `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentPeriods?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}&AssessClass=${classes}&Section=${selectsec}`;
+    const baseurl = `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentPeriods?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}&AssessClass=${classes}&Section=${selectsec}`;
     axios.get(`${baseurl}`).then((res) => {
       console.log("peri ", res);
       setPeriodData([...res.data]);
     });
   };
 
-  const getAreas = (period) => {
-    const baseurl =
-      `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentPeriodSubjects?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}&AssessClass=${classes}&Section=${section}&Period=${period}`;
+  const getArea = (period) => {
+    const baseurl = `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentPeriodSubjects?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}&AssessClass=${classes}&Section=${section}&Period=${period}`;
     axios.get(`${baseurl}`).then((res) => {
-      console.log("area", res);
-      setArea([...res.data]);
+      console.log("selarea", res);
+      setSelArea([...res.data]);
     });
   };
 
-  const getSubjects = () => {
-    const baseurl =
-      "http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentPeriodSubjects?UserMailId=admin.Bp@sas.com&AcademicYear=2020-2021&Branch=SLESBP&AssessClass=IV&Section=B&Period=Term%201";
+  const getSubject = () => {
+    const baseurl = `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentPeriodSubjects?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}&AssessClass=${classes}&Section=${section}&Period=${period}`;
     axios.get(`${baseurl}`).then((res) => {
       console.log("subj", res);
       setSubject([...res.data]);
+    
+    });
+  };
+
+  const getType = (subject) => {
+    const baseurl = `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessmentPeriodTestCategories?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}&AssessClass=${classes}&Section=${section}&Period=${period}&Subject=${subject}`;
+    axios.get(`${baseurl}`).then((res) => {
+      console.log("type", res);
+      setTypeD([...res.data]);
+    });
+  };
+
+
+
+  const getStudD = (type) => {
+    const baseurl = `http://183.82.99.10/Ae-Erp-Api_DH/Assessment/GetAssessment?UserMailId=${email}&AcademicYear=${year}&Branch=${enteredBranch}&AssessClass=${classes}&Section=${section}&Period=${period}&Subject=${subject}&catg=${type}`;
+    axios.get(`${baseurl}`).then((res) => {
+      
+      console.log("StudD", res,studD);
+      // setStudD([...res.data]);
     });
   };
 
@@ -99,49 +116,58 @@ function App() {
   const handleYearOnChange = (e) => {
     console.log("year", e.target.value);
     getenteredBranch(e.target.value);
-    setYear(e.target.value)
+    setYear(e.target.value);
   };
 
-   const handleBranchOnChange = async (e) => {
-     let email
+  const handleBranchOnChange = async (e) => {
+    let email;
     if (e.target.value === "SLESBP") {
-       setEmail("admin.bp@sas.com");
-       email="admin.bp@sas.com";
+      setEmail("admin.bp@sas.com");
+      email = "admin.bp@sas.com";
     } else if (e.target.value === "SLESKS") {
-       setEmail("admin.Ks@sas.com");
-       email="admin.Ks@sas.com";
+      setEmail("admin.Ks@sas.com");
+      email = "admin.Ks@sas.com";
     } else {
-       setEmail("admin.Mp@sas.com");
-       email="admin.Mp@sas.com";
+      setEmail("admin.Mp@sas.com");
+      email = "admin.Mp@sas.com";
     }
     console.log("branch", e.target.value);
 
     // getSection(e.target.value);
-    setenteredBranch(e.target.value)
-    getClasses(e.target.value,email)
+    setenteredBranch(e.target.value);
+    getClasses(e.target.value, email);
   };
 
   const handleClassOnChange = (value) => {
-    setClasses(value.target.value)
+    setClasses(value.target.value);
     console.log("class", value.target.value);
     getSection(value.target.value);
   };
 
   const hadleSectionOnChange = (value) => {
-    setSection(value.target.value)
+    setSection(value.target.value);
     console.log("sec", value.target.value);
     getPeriods(value.target.value);
   };
 
   const hadlePeriodOnChange = (e) => {
-    setPeriod(e.target.value)
-    console.log("area");
-    getAreas(e.target.value);
+    setPeriod(e.target.value);
+    console.log("period", e.target.value);
+    getArea(e.target.value);
+   
   };
 
-  const hadleAreaOnChange = () => {
-    console.log("area");
-    // getSubject()
+  const handleAreaOnChange = (e) => {
+    console.log("area", e.target.value);
+    // getArea(e.target.value);
+    getSubject(e.target.value)
+    getType(e.target.value)
+  };
+
+  const handleTypeOnChange = (e) => {
+    setAreaD(e.target.value);
+    console.log("Type", e.target.value);
+    getStudD(e.target.value);
   };
 
   return (
@@ -155,6 +181,7 @@ function App() {
           <div className="dropdown">
             <p id="conn">Academic Year</p>
             <select id="academic" onChange={(e) => handleYearOnChange(e)}>
+              <option value="0">Select AcademicYear</option>
               <option value="2019-2020">2019-2020</option>
               <option value="2020-2021">2020-2021</option>
               <option value="2021-2022">2021-2022</option>
@@ -162,19 +189,17 @@ function App() {
           </div>
           <div className="dropdown">
             <p id="conn">Branch</p>
-            <select
-              id="academic"
-              onChange={(e) => handleBranchOnChange(e)}
-             
-            >
+            <select id="academic" onChange={(e) => handleBranchOnChange(e)}>
+            <option value="0">Please select branch </option>
               <option value="SLESBP">St. Andrews School, Bowenpally</option>
               <option value="SLESKS">St. Andrews School, Keesara</option>
               <option value="LEETMP">St. Andrews School, Marredpally</option>
             </select>
           </div>
           <div className="dropdown">
-            <p id="conn" >Class</p>
+            <p id="conn">Class</p>
             <select id="academic" onChange={(e) => handleClassOnChange(e)}>
+            <option value="0">Select Class</option>
               {classesData.map((option) => (
                 <option value={option.U_VALUS}>{option.U_VALUS}</option>
               ))}
@@ -182,7 +207,9 @@ function App() {
           </div>
           <div className="dropdown">
             <p id="conn">Section</p>
+            
             <select id="academic" onChange={(e) => hadleSectionOnChange(e)}>
+            <option value="0">Select Section</option>
               {sectionData.map((option) => (
                 <option value={option.U_VALUS}>{option.U_VALUS}</option>
               ))}
@@ -191,47 +218,55 @@ function App() {
           <div className="dropdown">
             <p id="conn">Assessment Cycle</p>
             <select id="academic" onChange={(e) => hadlePeriodOnChange(e)}>
+            <option value="0">Select Assessment Cycle</option>
               {periodData.map((option) => (
                 <option value={option.U_VALUS}>{option.U_VALUS}</option>
               ))}
             </select>
           </div>
           &nbsp;
+          
           <div className="dropdown">
             <p id="conn">Assessment Area</p>
-            <select id="academic" onChange={() => hadleAreaOnChange()}>
-              {area.map((option) => (
+            <select id="academic" onChange={(e) => handleAreaOnChange(e)}>
+            <option value="0">Select Assessment Area</option>
+              {selarea.map((option) => (
                 <option value={option.StudFee}>{option.StudFee}</option>
               ))}
             </select>
           </div>
           <div className="dropdown">
             <p id="conn">Assessment Subject</p>
-            <select id="academic">
-            {area.map((option) => (
+            <select id="academic" onChange={(e) => handleAreaOnChange(e)}>
+            <option value="0">Select Assessment Subject</option>
+              {selarea.map((option) => (
                 <option value={option.U_VALUS}>{option.U_VALUS}</option>
               ))}
-             
-                            
             </select>
           </div>
           <div className="dropdown">
             <p id="conn">Assessment Type</p>
-            <select id="academic">
-              <option value="Select Assessment Type">
-                Select Assessment Type
-              </option>
+            <select id="academic" onChange={(e) => handleTypeOnChange(e)}>
+               <option value="0">Select Assessment Type</option>
+          
+              {typeD.map((option) => (
+                <option value={option.U_VALUS}>{option.U_VALUS}</option>
+              ))}
             </select>
           </div>
           <div className="dropdown">
             <p id="conn">Students</p>
-            <select id="academic">
-              <option value="0">Select Assessment Type</option>
+            <select id="academic" onChange={(e) => handleTypeOnChange(e)}>
+              <option value="0">Select Student</option>
+              {studD.map((option) => (
+                <option value={option.U_VALUS}>{option.U_VALUS}</option>
+              ))}
             </select>
           </div>
           <form id="form1">
             <input id="btnad" type="submit" value="Search" />
           </form>
+          
         </div>
         <div className="card-body"></div>
       </div>
